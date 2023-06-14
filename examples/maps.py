c37cc3dfdaf51a2370a5ea81a1ad53d9ba8fa56e
Aki Van Ness <aki@lethalbit.net>
2023-06-14 11:02:58 -0400

    updated maps example

--- examples/maps.py
+++ examples/maps.py
@@ -3,6 +3,9 @@
 
 from sycophant.proc import maps
 
+
 # Print all memory maps for the current process
 for entry in  maps.all():
-    print(f'{entry.start:016X}-{entry.end:016X} from {entry.path}')
+    print(f'Mapping: { entry.path if entry.is_backed() else "ANONYMOUS" }')
+    print(f'  Range: {entry.start:016X}:{entry.end:016X}')
+    print(f'  Prot:  {entry.flags}')
